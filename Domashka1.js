944630000331dd3bebe532f154042d16acc358a0
mysccafrte <80043850+mysccafrte@users.noreply.github.com>
2021-03-04 15:40:28 +0300

    Add files via upload

---
+++ Domashka1.js
@@ -0,0 +1,12 @@
+let printnum = function(num){
+    for (let i = 1; i <= num; i++){
+
+        if(i === 5) console.log("Пять");
+        else if(i === 13) console.log("Тринадцать");
+        else if(i === 22) console.log("Двадцать два");
+        else if(i === 35) console.log("Тридцать пять");
+        else if(i === 98) console.log("Девяносто восемь");
+        else console.log(i);
+
+    }
+}
